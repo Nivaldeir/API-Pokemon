3910b2a8d317ecd2551e5c825906bf124df5ee74
Nivaldeir <Nivaldeir-silva@hotmail.com>
2022-04-22 14:03:03 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <div class="column is-half is-offset-one-quarter">
+      <input type="text" class="input is-rounde" style="margin-bottom: 2%; " placeholder="Pesquisar" v-model="busca">
+      <button class="button is-fullwidth is-success">Buscar</button>
+      <div v-for="(poke, index) in resultadoBusca" :key="index">
+        <PokeMon :name="poke.name" :url="poke.url" :num="index + 1" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import PokeMon from "./components/PokeMon";
+
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+      busca: '',
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        this.pokemons = res.data.results;
+      })
+      .catch((e) => console.log(e));
+  },
   components: {
-    HelloWorld
+    PokeMon,
+  },
+  computed:{
+    resultadoBusca: function(){
+      if(this.busca == ''|| this.busca == ` `){
+        return this.pokemons;
+      }else{
+        console.log(this.busca)
+       
+        return this.pokemons.filter(p =>{p.name == this.busca})
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
